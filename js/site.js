796a7675c59dd84ce912cbc4cd7bbfb6fc23bd61
Adrian <32913236+adrigski@users.noreply.github.com>
2021-10-18 23:04:03 -0500

    created js submit alert

--- js/site.js
+++ js/site.js
@@ -1,7 +1,14 @@
-var js_available = document.getElementsByTagName('html');
+var js_available = document.getElementsByTagName('html')[0];
+var form = document.getElementById("signup_form");
 
 function js_ready(){
     js_available.setAttribute('class', 'hasjs');
 }
 
+function clickbutton(){
+    alert("Thank you taking a interest in our game, we hope to see you in the future beta!");
+}
+
 window.addEventListener('load', js_ready);
+form.addEventListener("submit", clickbutton);
+
