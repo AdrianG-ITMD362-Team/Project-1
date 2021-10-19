71b9c41fcb0cd5512b66b7d916d8b37af0ee445f
Adrian <32913236+adrigski@users.noreply.github.com>
2021-10-18 23:52:13 -0500

    changed some padding and fixed formatting

--- js/site.js
+++ js/site.js
@@ -2,11 +2,11 @@ var js_available = document.getElementsByTagName('html')[0];
 var form = document.getElementById("signup_form");
 
 function js_ready(){
-    js_available.setAttribute('class', 'hasjs');
+  js_available.setAttribute('class', 'hasjs');
 }
 
 function clickbutton(){
-    alert("Thank you taking a interest in our game, we hope to see you in the future beta!");
+  alert("Thank you taking a interest in our game, we hope to see you in the future beta!");
 }
 
 window.addEventListener('load', js_ready);
